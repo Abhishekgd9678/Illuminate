61295ed86e2f8d262ca7bff5dbd2c21a77c1b69f
Abhishekgd9678 <abhishekgd2003@gmail.com>
2024-01-18 19:41:36 +0530

    body

--- FrontEnd/src/Body.js
+++ FrontEnd/src/Body.js
@@ -3,11 +3,61 @@ import event from '../images/event_logo.png'
 
 const Body = () => {
   return (
-    <div className='m-4'>
 
-  
-   
+<>
+<div className='m-4 mb-0 mt-10 h-[50vh] relative flex justify-center items-center  *:m-4 back'>
+  <div className='w-[50vw] flex flex-col justify-center items-center text-center relative z-10 rounded-xl p-4 overflow-hidden border-4 border-gradient animate-border-gradient'>
+    <div>
+      <h4 className='text-xl font-bold text-white '>The National Institute of Engineering In Collaboration With L & T Technology Services </h4>
+      <h4 className='text-white text-xl'>Presents</h4>
     </div>
+  </div>
+
+
+  <div className="">
+            <img src={event} alt="" className="w-full h-full object-cover" />
+          </div>
+</div>
+
+
+  <div className='flex m-4 justify-center items-center *:m-5 *:mx-28 z-50 *:border-black *:border  *:rounded-xl *:p-4'>
+    <div className='*:m-auto *:text-2xl z-10  bg-gradient-to-r from-cyan-500 to-blue-500  text-white'>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+  <path d="M12.75 12.75a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM7.5 15.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5ZM8.25 17.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM9.75 15.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5ZM10.5 17.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM12 15.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5ZM12.75 17.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM14.25 15.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5ZM15 17.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM16.5 15.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5ZM15 12.75a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM16.5 13.5a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z" />
+  <path fill-rule="evenodd" d="M6.75 2.25A.75.75 0 0 1 7.5 3v1.5h9V3A.75.75 0 0 1 18 3v1.5h.75a3 3 0 0 1 3 3v11.25a3 3 0 0 1-3 3H5.25a3 3 0 0 1-3-3V7.5a3 3 0 0 1 3-3H6V3a.75.75 0 0 1 .75-.75Zm13.5 9a1.5 1.5 0 0 0-1.5-1.5H5.25a1.5 1.5 0 0 0-1.5 1.5v7.5a1.5 1.5 0 0 0 1.5 1.5h13.5a1.5 1.5 0 0 0 1.5-1.5v-7.5Z" clip-rule="evenodd" />
+</svg>
+
+<h4>
+  17th January , 2023
+</h4>
+    </div>
+    <div className='*:m-auto *:text-2xl  bg-gradient-to-r from-cyan-500 to-[#5eb1e9]  text-white'>
+    
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+  <path fill-rule="evenodd" d="m11.54 22.351.07.04.028.016a.76.76 0 0 0 .723 0l.028-.015.071-.041a16.975 16.975 0 0 0 1.144-.742 19.58 19.58 0 0 0 2.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 0 0-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 0 0 2.682 2.282 16.975 16.975 0 0 0 1.145.742ZM12 13.5a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" clip-rule="evenodd" />
+</svg>
+
+
+<h4>
+Karnataka State Open University
+</h4>
+
+</div>
+<div className='*:m-auto  *:text-2xl   bg-gradient-to-r from-cyan-500 to-blue-500  text-white'>
+
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+  <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 6a.75.75 0 0 0-1.5 0v6c0 .414.336.75.75.75h4.5a.75.75 0 0 0 0-1.5h-3.75V6Z" clip-rule="evenodd" />
+</svg>
+
+   <h4>
+   10:30 AM to 1 PM
+   </h4>
+  </div>
+ </div>
+
+ 
+   </>
+  
  
   )
 }
