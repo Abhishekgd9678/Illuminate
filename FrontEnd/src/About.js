18add10f6e4c635faacfe8f9c86e0ab66a12928c
Abhishekgd9678 <abhishekgd2003@gmail.com>
2024-01-18 23:04:32 +0530

    Speakers left out

--- FrontEnd/src/About.js
+++ FrontEnd/src/About.js
@@ -13,9 +13,9 @@ const About = () => {
 
     <div class="flex flex-wrap items-center mt-20 text-left text-center">
         <div class="w-full md:w-3/5 lg:w-1/2 px-4 flex justify-center items-center">
-            <img src={logonie} className='w-[300px]'/>
+            <img src={logonie} className='h-[250px]'/>
         </div>
-        <div class="w-full md:w-2/5 lg:w-1/2 px-4 text-center md:text-left lg:pl-12">
+        <div class="w-full md:w-2/5 lg:w-1/2 px-4 text-center md:text-left lg:pl-12 bg-gradient-to-r from-slate-300 to bg-slate-100 p-5">
             <h3 class="font-bold mt-8 text-xl md:mt-0 sm:text-2xl">
                The National Institute of Engineering
             </h3>
@@ -29,11 +29,11 @@ const About = () => {
         <div class="w-full md:w-3/5 lg:w-1/2 px-4">
             <img src={lt}/>
         </div>
-        <div class="w-full md:w-2/5 lg:w-1/2 px-4 md:order-first text-center md:text-left lg:pr-12">
-            <h3 class="font-bold mt-8 text-xl md:mt-0 sm:text-2xl">
-                Easy collaboration
+        <div class="w-full md:w-2/5 lg:w-1/2 px-4 md:order-first text-center md:text-left lg:pr-12 bg-gradient-to-r from-slate-300 to bg-slate-100 p-5">
+            <h3 class="font-bold mt-8 text-xl md:mt-0 sm:text-2xl text-right">
+            L&T Technology Services
             </h3>
-            <p class="sm:text-lg mt-6">
+            <p class="sm:text-lg mt-6 ">
             L&T Technology Services Limited (LTTS) is a global leader in Engineering and R&D (ER&D) services. With close to 1,000 patents filed for 57 of the Global Top 100 ER&D spenders, LTTS lives and breathes engineering. Our innovations speak for themselves – World’s 1st Autonomous Welding Robot, Solar ‘Connectivity’ Drone, and the Smartest Campus in the World, to name a few.
 
 LTTS’ expertise in engineering design, product development, smart manufacturing, and digitalization touches every area of our lives. With over 100 Innovation and R&D design centers globally, we specialize in disruptive technology spaces including 5G, Artificial Intelligence, Collaborative Robots, Digital Factory, and Autonomous Transportation.
--- FrontEnd/src/Timeline.js
+++ FrontEnd/src/Timeline.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import event from '../images/event_logo.png'
+import b from '../images/2.jpg'
 const Timeline = () => {
   return (
    <>
-<section>
-    <div class="bg-[#191943] text-black py-8 timeline m-5">
+ <section >
+    <div class="bg-[#191943] text-black py-8  m-5 relative" style={{ backgroundImage: `url(${b})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
     <div class="container mx-auto flex flex-col items-start md:flex-row my-12 md:my-24">
-      <div class="flex flex-col w-full sticky md:top-36 lg:w-1/3 mt-2 md:mt-12 px-8">
+      <div class="flex flex-col w-full sticky md:top-36 lg:w-1/3 mt-2 md:mt-12 px-8 ">
    
         <p class="text-3xl md:text-4xl leading-normal md:leading-relaxed mb-2 text-white">Schedule</p>
        <p class="text-3xl md:text-4xl leading-normal md:leading-relaxed mb-2 text-white">Illuminate -2024</p>
-       <img class="mx-auto   " src={event} />
+       <img class="mx-auto my-4   " src={event} />
       </div>
     
       <div class="ml-0 md:ml-12 lg:w-2/3 sticky">
@@ -23,32 +24,30 @@ const Timeline = () => {
             <div class="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
               <div class="order-1 w-5/12"></div>
               <div class="order-1 w-5/12 px-1 py-4 text-right">
-                <p class="mb-3 text-base text-white">1-6 May, 2021</p>
-                <h4 class="mb-3 font-bold text-lg md:text-2xl text-white">Registration</h4>
+                <p class="mb-3 text-base text-white">Schedule 1</p>
+                <h4 class="mb-3 font-bold text-lg md:text-2xl text-white">Name</h4>
                 <p class="text-sm md:text-base leading-snug text-gray-50 text-opacity-100">
-                 
+                 description
                 </p>
               </div>
             </div>
             <div class="mb-8 flex justify-between items-center w-full right-timeline">
               <div class="order-1 w-5/12"></div>
               <div class="order-1  w-5/12 px-1 py-4 text-left">
-                <p class="mb-3 text-base text-white">6-9 May, 2021</p>
-                <h4 class="mb-3 font-bold text-lg md:text-2xl text-white">Participation</h4>
+                <p class="mb-3 text-base text-white">Schedule 2</p>
+                <h4 class="mb-3 font-bold text-lg md:text-2xl text-white">Name</h4>
                 <p class="text-sm md:text-base leading-snug text-gray-50 text-opacity-100">
-                  Participate online. The links for your registered events will be sent to you via email and whatsapp
-                  groups. Use those links and show your talent.
+                 description
                 </p>
               </div>
             </div>
             <div class="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
               <div class="order-1 w-5/12"></div>
               <div class="order-1 w-5/12 px-1 py-4 text-right">
-                <p class="mb-3 text-base text-white"> 10 May, 2021</p>
-                <h4 class="mb-3 font-bold text-lg md:text-2xl text-white">Result Declaration</h4>
+                <p class="mb-3 text-base text-white">schedule 3</p>
+                <h4 class="mb-3 font-bold text-lg md:text-2xl text-white">Name</h4>
                 <p class="text-sm md:text-base leading-snug text-gray-50 text-opacity-100">
-                  The ultimate genius will be revealed by our judging panel on 10th May, 2021 and the resukts will be
-                  announced on the whatsapp groups and will be mailed to you.
+                 description
                 </p>
               </div>
             </div>
@@ -57,11 +56,10 @@ const Timeline = () => {
               <div class="order-1 w-5/12"></div>
 
               <div class="order-1  w-5/12 px-1 py-4">
-                <p class="mb-3 text-base text-white">12 May, 2021</p>
-                <h4 class="mb-3 font-bold  text-lg md:text-2xl text-left text-white">Prize Distribution</h4>
+                <p class="mb-3 text-base text-white">Schedule 4</p>
+                <h4 class="mb-3 font-bold  text-lg md:text-2xl text-left text-white">Name</h4>
                 <p class="text-sm md:text-base leading-snug text-gray-50 text-opacity-100">
-                  The winners will be contacted by our team for their addresses and the winning goodies will be sent at
-                  their addresses.
+                  description
                 </p>
               </div>
             </div>
