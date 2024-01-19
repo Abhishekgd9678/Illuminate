0965aa53814dd42b45a7761223bd8d21406f4aa5
Ajay-S-Biradar <sbmunnu@gmail.com>
2024-01-20 03:13:11 +0530

    made speaker.js body.js why.js  reponsive for small devices

--- FrontEnd/src/Register.js
+++ FrontEnd/src/Register.js
@@ -14,7 +14,7 @@ const LoginPage = () => {
               <div className="g-0 lg:flex lg:flex-wrap">
                 {/* Left column container */}
                 <div className="px-4 md:px-0 lg:w-6/12">
-                  <div className="md:mx-6 md:p-12">
+                  <div className="md:mx-6 md:p-12 m-0 p-0 ">
                     {/* Logo */}
                     <div className="text-center">
                       
--- FrontEnd/src/header.js
+++ FrontEnd/src/header.js
@@ -10,12 +10,12 @@ const Header = () => {
           <img className="w-[50px]" src={logonie} />
         </div>
         <div>
-          <h4 className="ml-2 font-bold text-xl">The National Institute of Engineering</h4>
+          <h4 className="hidden md:flex ml-2 font-bold text-xl">The National Institute of Engineering</h4>
         </div>
         </div>
         <div className="flex justify-center items-center">
             <div>
-                <img className="h-[50px]" src={lt}/>
+                <img className="h-[50px] md:w-auto w-14 object-cover object-left" src={lt}/>
             </div>
            
         </div>
@@ -23,12 +23,12 @@ const Header = () => {
       </div>
 
 
-      <div className="flex justify-center ">
-        <ul className=" flex items-center *:px-4">
+      <div className="flex justify-center w-full">
+        <ul className=" flex felx-row font-medium w-10/12 md:w-full md:justify-around md:text-2xl ">
         <Link to=""><li>Why Attend?</li></Link>
         <Link to="/about"><li>Who are We?</li></Link>
-        <Link to="/speaker"><li>Speakers</li></Link>
-        <Link to="/schedule"><li>Schedule</li></Link>
+        <Link to="/speaker"><li>Speakers </li></Link>
+        <Link to="/schedule"><li>Schedule </li></Link>
         <Link to="/register"><li>Register</li></Link>
 
         </ul>
